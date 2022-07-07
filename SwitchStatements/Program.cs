13659d5bf2791a455b19cb89e0b1c21ee8f7ce49
Enmanuel Ozoria <ozoriaeman1998@gmail.com>
2022-07-07 13:05:59 -0400

    switch statements exercise

---
+++ SwitchStatements/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace SelectionStatements
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("What is your favorite school subject?");
+            var favSubject = Console.ReadLine().ToLower();
+
+            switch (favSubject)
+            {
+                case "english":
+                    Console.WriteLine("English is boring.");
+                    break;
+                case "math":
+                    Console.WriteLine("Math is essential!");
+                    break;
+                case "science":
+                    Console.WriteLine("Science is everywhere!");
+                    break;
+                case "social studies":
+                    Console.WriteLine("Social Studies is alright.");
+                    break;
+                case "art":
+                    Console.WriteLine("Art is creative!");
+                    break;
+                default:
+                    Console.WriteLine("Hmm.. I don't recognize that subject.");
+                    break;
+            }
+        }
+    }
+}
